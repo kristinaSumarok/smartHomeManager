4a67f6432ddcb1847c54b45acc6431687cb3db76
Ctaekh <jurkev@gmail.com>
2024-10-17 14:26:05 +0300

    feat: automatic date persistance

--- api/src/Domain/Common/AuditableEntity.cs
+++ api/src/Domain/Common/AuditableEntity.cs
@@ -1,9 +1,14 @@
-﻿namespace Homemap.Domain.Common
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homemap.Domain.Common
 {
     public abstract class AuditableEntity : Entity
     {
+        [Required]
         public DateTime CreatedAt { get; set; }
 
+        [Required]
         public DateTime LastModifiedAt { get; set; }
     }
 }
--- api/src/Infrastructure/Infrastructure.Data/Repositories/Repository.cs
+++ api/src/Infrastructure/Infrastructure.Data/Repositories/Repository.cs
@@ -1,20 +1,38 @@
 ﻿using Homemap.ApplicationCore.Interfaces.Repositories;
+using Homemap.Domain.Common;
 using Homemap.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Homemap.Infrastructure.Data.Repositories
 {
-    internal class Repository : IRepository
+    internal class Repository(ApplicationDbContext context) : IRepository
     {
-        private readonly ApplicationDbContext _context;
-
-        public Repository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        private readonly ApplicationDbContext _context = context;
 
         public async Task<bool> SaveAsync()
         {
+            UpdateTimestamps();
             return await _context.SaveChangesAsync().ConfigureAwait(false) > 0;
+        }
+
+        private void UpdateTimestamps()
+        {
+            var entries = _context.ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is AuditableEntity && 
+                            (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            foreach (var entry in entries)
+            {
+                var auditableEntity = (AuditableEntity)entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    auditableEntity.CreatedAt = DateTime.UtcNow;
+                }
+
+                auditableEntity.LastModifiedAt = DateTime.UtcNow;
+            }
         }
     }
 }
