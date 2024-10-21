f749b46188bde55c5c1087a7abf882e695625573
kristina Troitski <ktroitski33@gmail.com>
2024-10-21 16:55:52 +0300

    feat(fetch-receivers): move receiver list to components folder, add success check

--- app/pages/projects/[id]/index.vue
+++ app/pages/projects/[id]/index.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-const receiversStore = useReceiversStore()
-const { receivers } = storeToRefs(receiversStore)
-
-await useLazyAsyncData(() => receiversStore.getReceivers())
 </script>
 
 <template>
   <div>
-    <h3>Receivers:</h3>
-    <ul>
-      <li
-        v-for="receiver in receivers"
-        :key="receiver.id"
-      >
-        <h3>
-          {{ receiver.name }}
-        </h3>
-      </li>
-    </ul>
+    <h2>Overview</h2>
+    <ReceiverList />
   </div>
 </template>
