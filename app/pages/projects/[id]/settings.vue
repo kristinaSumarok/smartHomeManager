2811da5a5f56221054673dfac61301ce9d426292
kristina Troitski <ktroitski33@gmail.com>
2024-10-22 00:18:16 +0300

    refactor(delete-project): add 'Settings' text

--- app/pages/projects/[id]/settings.vue
+++ app/pages/projects/[id]/settings.vue
@@ -22,6 +22,7 @@ async function handleRemove() {
 
 <template>
   <div>
+    Settings
     <button
       class="h-9 inline-flex items-center justify-center border rounded-md bg-blue-600 px-3 text-sm text-white font-semibold hover:bg-blue-500 focus-visible:(outline-blue-600 ring ring-white ring-inset)"
       @click="handleRemove()"
