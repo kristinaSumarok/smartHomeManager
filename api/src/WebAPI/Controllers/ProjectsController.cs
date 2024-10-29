64c27aba12e8a3b2cd2af0fbc8a392e698a66839
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-10-29 22:14:23 +0200

    feat(remove-base-controller): update controllers and remove base controller

--- api/src/WebAPI/Controllers/ProjectsController.cs
+++ api/src/WebAPI/Controllers/ProjectsController.cs
@@ -1,13 +1,102 @@
-﻿using FluentValidation;
+﻿using ErrorOr;
+using FluentValidation;
 using Homemap.ApplicationCore.Interfaces.Services;
 using Homemap.ApplicationCore.Models;
+using Homemap.WebAPI.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Homemap.WebAPI.Controllers
 {
-    public class ProjectsController : BaseController<ProjectDto>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
     {
-        public ProjectsController(IService<ProjectDto> service, IValidator<ProjectDto> validator) : base(service, validator)
+        private readonly IService<ProjectDto> _service;
+
+        private readonly IValidator<ProjectDto> _validator;
+
+        public ProjectsController(IService<ProjectDto> service, IValidator<ProjectDto> validator)
         {
+            _service = service;
+            _validator = validator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IReadOnlyList<ProjectDto>>> GetAll()
+        {
+            return Ok(await _service.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProjectDto>> GetById(int id)
+        {
+            ErrorOr<ProjectDto> dtoOrError = await _service.GetByIdAsync(id);
+
+            if (dtoOrError.IsError)
+                return this.ErrorOf(dtoOrError.FirstError);
+
+            return dtoOrError.Value;
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ProjectDto>> Create([FromBody] ProjectDto dto)
+        {
+            var validationResult = await _validator.ValidateAsync(dto);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.AddToModelState(ModelState);
+
+                return BadRequest(ModelState);
+            }
+
+            dto = await _service.CreateAsync(dto);
+
+            return CreatedAtAction(nameof(Create), new { dto.Id }, dto);
+        }
+
+        [HttpPut("{id}")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ProjectDto>> Update(int id, [FromBody] ProjectDto dto)
+        {
+            var validationResult = await _validator.ValidateAsync(dto);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.AddToModelState(ModelState);
+
+                return BadRequest(ModelState);
+            }
+
+            ErrorOr<ProjectDto> dtoOrError = await _service.UpdateAsync(id, dto);
+
+            if (dtoOrError.IsError)
+                return this.ErrorOf(dtoOrError.FirstError);
+
+            return dtoOrError.Value;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            ErrorOr<Deleted> deletedOrError = await _service.DeleteAsync(id);
+
+            if (deletedOrError.IsError)
+                return this.ErrorOf(deletedOrError.FirstError);
+
+            return NoContent();
         }
     }
 }
