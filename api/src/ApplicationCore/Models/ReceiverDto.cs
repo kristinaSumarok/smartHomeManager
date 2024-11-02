4fae1e4400a061d933452819f046add586aa134d
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-11-02 11:20:30 +0200

    fix(dtos): allow receiver name to be autogenerated

--- api/src/ApplicationCore/Models/ReceiverDto.cs
+++ api/src/ApplicationCore/Models/ReceiverDto.cs
@@ -5,7 +5,5 @@ namespace Homemap.ApplicationCore.Models
     public record ReceiverDto : AuditableEntityDto
     {
         public required string Name { get; init; }
-
-        public IReadOnlyCollection<DeviceDto> Devices { get; init; } = [];
     }
 }
--- api/src/ApplicationCore/Validators/ReceiverDtoValidator.cs
+++ api/src/ApplicationCore/Validators/ReceiverDtoValidator.cs
@@ -8,16 +8,7 @@ namespace Homemap.ApplicationCore.Validators
         public ReceiverDtoValidator()
         {
             RuleFor(receiver => receiver.Name)
-                .NotEmpty().WithMessage("Receiver name is required.")
                 .MaximumLength(100).WithMessage("Receiver name must not exceed 100 characters.");
-
-            //for test purposes
-            RuleFor(receiver => receiver.Devices)
-                .NotNull().WithMessage("Devices collection cannot be null.")
-                .Must(devices => devices.Count > 0).WithMessage("Devices collection must contain at least one device.");
-
-            RuleForEach(receiver => receiver.Devices)
-                .SetValidator(new DeviceDtoValidator());
         }
     }
 }
