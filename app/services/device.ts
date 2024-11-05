b29810e67eef446b054949c9d9e83eec6c1de8fd
kristina Troitski <ktroitski33@gmail.com>
2024-11-05 17:57:37 +0200

    feat(display-devices): add device.ts to services

---
+++ app/services/device.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+import { deviceSchema } from '~/domain/device'
+import type { Receiver } from '~/domain/receiver'
+
+export const useDeviceService = () => {
+  const config = useRuntimeConfig()
+  const repository = createDeviceRepository(
+    $fetch.create({
+      baseURL: `${config.public.apiBaseUrl}`,
+    }),
+  )
+
+  return {
+    async getDevices(receiverId: Receiver['id']) {
+      const response = await repository.findAll(receiverId)
+
+      return z.array(deviceSchema).parse(response)
+    },
+  }
+}
--- app/utils/repository.ts
+++ app/utils/repository.ts
@@ -1,2 +1,3 @@
 export { createProjectRepository } from './repository/project'
 export { createReceiverRepository } from './repository/receiver'
+export { createDeviceRepository } from './repository/device'
