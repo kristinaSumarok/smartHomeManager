b63978b459755c1d6eaa8872844db1f5dbc7ad64
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-11-07 16:16:28 +0200

    fix(ui-fix): extract project list to standalone component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import { NuxtLink } from '#components'
-
-const projectsStore = useProjectsStore()
-const { projects } = storeToRefs(projectsStore)
-
-const { status } = await useLazyAsyncData(() => projectsStore.getProjects())
 </script>
 
 <template>
@@ -21,31 +16,7 @@ const { status } = await useLazyAsyncData(() => projectsStore.getProjects())
       </p>
     </div>
     <div class="mt-8 max-w-3xl w-full">
-      <ul
-        v-if="status === 'success'"
-        role="list"
-        class="grid cols-1 gap-2 lg:cols-3 sm:cols-2"
-      >
-        <li
-          v-for="project in projects"
-          :key="project.id"
-        >
-          <ProjectCard :project="project" />
-        </li>
-      </ul>
-      <div
-        v-else
-        class="grid gap-2 lg:cols-3 sm:cols-2"
-      >
-        <div
-          v-for="i in 6"
-          :key="i"
-          class="min-w-64 border border-zinc-950/10 rounded-lg bg-white px-4 py-2 shadow-sm space-y-2"
-        >
-          <div class="h-4 w-2/3 animate-pulse rounded bg-zinc-200" />
-          <div class="h-4 w-1/3 animate-pulse rounded bg-zinc-200" />
-        </div>
-      </div>
+      <ProjectList />
     </div>
     <div class="mt-10 max-w-2xl w-full flex flex-col gap-2 sm:(flex-row items-center justify-center)">
       <Button
